50c6fb5333ef677718c44e1ada6b8f8c2050cb51
Harry Theo <harrytheo@google.com>
2025-01-22 17:01:31 +0000

    Updated license.

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 require('dotenv').config()
 const path = require('path')
 
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { exec } = require('child_process');
 const fs = require('fs');
